4cf52c30d24c80b1617e14432706e404a9a35f89
oudream <oudream@126.com>
2020-04-10 16:52:59 +0800

    Create readme.sh

---
+++ man/readme.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# http://groovy-lang.org/single-page-documentation.html
+# http://groovy-lang.org/documentation.html
+# https://groovy-lang.org/syntax.html
+# https://groovy-lang.org/objectorientation.html
+# https://groovy-lang.org/closures.html
+
+
+# https://www.w3cschool.cn/groovy/
+
+
+# https://www.tutorialspoint.com/groovy/
+# https://code-maven.com/groovy
+
+
+# https://docs.groovy-lang.org/latest/html/api/groovy/lang/Closure.html
+# https://www.tutorialspoint.com/groovy/groovy_closures.htm
